76d009c0121ed3f50b897d998687ddaa9669b549
murad <murradkhalil@gmail.com>
2020-10-09 14:17:17 -0700

    redirecting unknown urls

--- myrails/urls.py
+++ myrails/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.conf.urls import re_path
 
 urlpatterns = [
     path('', include('myapp.urls')),
     path('admin/', admin.site.urls),
+    re_path("", RedirectView.as_view(url='/')),
+
 ]
