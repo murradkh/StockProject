fa338a5b880f65b726bd8784d2f39226e9263aeb
murad <murradkhalil@gmail.com>
2020-10-09 09:14:22 -0700

    handling non_existent symbol stock and Service-unreachable exceptions

--- myapp/stock_api.py
+++ myapp/stock_api.py
@@ -1,8 +1,11 @@
+from .exceptions.stock_service import StockServerUnReachable, StockSymbolNotFound
+from requests.exceptions import ConnectionError
 import requests
 
 # Sandbox API - FOR TESTING
 BASE_URL = 'https://sandbox.iexapis.com'
 PUBLIC_TOKEN = 'Tpk_c818732500c24764801eb121fa658bb6'
+
 
 # Real API - FOR PRODUCTION
 # YOU NEED TO CREATE AN ACCOUNT TO RECEIVE YOUR OWN API KEYS (its free)
@@ -16,34 +19,42 @@ PUBLIC_TOKEN = 'Tpk_c818732500c24764801eb121fa658bb6'
 # For all symbols with prices (*free weight*)
 # https://cloud.iexapis.com/stable/tops/last?token=pk_dd07f5a1aaea4a039cfe8118f3d9727a
 def _request_data(url, filter='', additional_parameters={}):
-	final_url = BASE_URL + url
+    final_url = BASE_URL + url
 
-	query_strings = {
-		'token': PUBLIC_TOKEN
-	}
-	query_strings.update(additional_parameters)
+    query_strings = {
+        'token': PUBLIC_TOKEN
+    }
+    query_strings.update(additional_parameters)
 
-	if filter:
-		query_strings['filter'] = filter
+    if filter:
+        query_strings['filter'] = filter
 
-	response = requests.get(final_url, params=query_strings)
+    response = requests.get(final_url, params=query_strings)
 
-	if not response.ok:
-		raise Exception('Unexpected response: ', response.__dict__)
-	return response.json()
+    if not response.ok:
+        raise Exception('Unexpected response: ', response.__dict__)
+    return response.json()
 
 
 def _get_top_stocks():
-	return _request_data('/stable/stock/market/list/mostactive',
-							  filter='symbol,companyName,latestVolume,change,changePercent,primaryExchange,marketCap,latestPrice,calculationPrice',
-							  additional_parameters={'displayPercent': 'true', 'listLimit': '20'})
+    return _request_data('/stable/stock/market/list/mostactive',
+                         filter='symbol,companyName,latestVolume,change,changePercent,primaryExchange,marketCap,latestPrice,calculationPrice',
+                         additional_parameters={'displayPercent': 'true', 'listLimit': '20'})
 
 
 def get_stock_info(symbol):
-	# 'symbol,companyName,marketcap,totalCash,primaryExchange,latestPrice,latestSource,change,changePercent'
-	return _request_data('/stable/stock/{symbol}/quote'.format(symbol=symbol),
-							  additional_parameters={'displayPercent': 'true'})
+    # 'symbol,companyName,marketcap,totalCash,primaryExchange,latestPrice,latestSource,change,changePercent'
+    try:
+        return _request_data('/stable/stock/{symbol}/quote'.format(symbol=symbol),
+                             additional_parameters={'displayPercent': 'true'})
+    except ConnectionError:
+        raise StockServerUnReachable("Stock server UnReachable!")
+    except Exception as e:
+        for arg in e.args:
+            if isinstance(arg, dict) and b"Unknown symbol" in arg.values():
+                raise StockSymbolNotFound("Unknown Stock Symbol!")
+        raise e
 
 
 def get_stock_historic_prices(symbol, time_range='1m'):
-	return _request_data('/stable/stock/{symbol}/chart/{time_range}'.format(symbol=symbol, time_range=time_range))
+    return _request_data('/stable/stock/{symbol}/chart/{time_range}'.format(symbol=symbol, time_range=time_range))
--- myapp/views.py
+++ myapp/views.py
@@ -4,48 +4,64 @@ from myapp.models import Stock
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
+from .exceptions.stock_service import StockServerUnReachable, StockSymbolNotFound
 
 
 # View for the home page - a list of 20 of the most active stocks
 def index(request):
-	# Query the stock table, filter for top ranked stocks and order by their rank.
-	data = Stock.objects.filter(top_rank__isnull=False).order_by('top_rank')
-	return render(request, 'index.html', {'page_title': 'Main', 'data': data })
+    # Query the stock table, filter for top ranked stocks and order by their rank.
+    data = Stock.objects.filter(top_rank__isnull=False).order_by('top_rank')
+    return render(request, 'index.html', {'page_title': 'Main', 'data': data})
 
 
 # View for the single stock page
 # symbol is the requested stock's symbol ('AAPL' for Apple)
 def single_stock(request, symbol):
-	data = stock_api.get_stock_info(symbol)
-	return render(request, 'single_stock.html', {'page_title': 'Stock Page - %s' % symbol, 'data': data})
+    try:
+        data = stock_api.get_stock_info(symbol)
+    except StockSymbolNotFound as e:
+        response = render(request, 'exception.html', {'404_error_message': e.message})
+        response.status_code = 404  # not found code
+        return response
+    except StockServerUnReachable as e:
+        response = render(request, 'exception.html', {'503_error_message': e.message})
+        response.status_code = 503  # Service Unavailable code
+        return response
+    except Exception as e:
+        response = render(request, 'exception.html',
+                          {'404_error_message': "Unknown Error occurred: {}".format(", ".join(e.args))})
+        response.status_code = 404  # Service Unavailable code
+        return response
+    else:
+        return render(request, 'single_stock.html', {'page_title': 'Stock Page - %s' % symbol, 'data': data})
 
 
 def register(request):
-	# If post -> register the user and redirect to main page
-	if request.method == 'POST':
-		firstname = request.POST.get('firstname')
-		lastname = request.POST.get('lastname')
-		email = request.POST.get('email')
-		password = request.POST.get('password')
+    # If post -> register the user and redirect to main page
+    if request.method == 'POST':
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
 
-		newuser = User.objects.create_user(username=email, email=email, password=password)
-		newuser.first_name = firstname
-		newuser.last_name = lastname
-		newuser.save()
-		return redirect('index')
-	else:
-		# If not post (regular request) -> render register page
-		return render(request, 'register.html', {'page_title': 'Register'})
+        newuser = User.objects.create_user(username=email, email=email, password=password)
+        newuser.first_name = firstname
+        newuser.last_name = lastname
+        newuser.save()
+        return redirect('index')
+    else:
+        # If not post (regular request) -> render register page
+        return render(request, 'register.html', {'page_title': 'Register'})
 
 
 def logout_view(request):
-	logout(request)
-	return redirect('index')
+    logout(request)
+    return redirect('index')
 
 
 # API for a stock's price over time
 # symbol is the requested stock's symbol ('AAPL' for Apple)
 # The response is JSON data of an array composed of "snapshot" objects (date + stock info + ...), usually one per day
 def single_stock_historic(request, symbol):
-	data = stock_api.get_stock_historic_prices(symbol, time_range='1m')
-	return JsonResponse({'data': data})
+    data = stock_api.get_stock_historic_prices(symbol, time_range='1m')
+    return JsonResponse({'data': data})
