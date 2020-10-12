11cb03973d59e6ec28f445a117e18fd981dea4dd
murad <murradkhalil@gmail.com>
2020-10-12 10:00:55 -0700

    ...

--- myrails/views.py
+++ myrails/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 def page_not_found(request):
     status_code = 404
-    resp = render(request, "exception.html", {'error_message': "Page Not Found!", "status_code": status_code})
-    resp.status_code = status_code
-    return resp
+    response = render(request, "exception.html", {'error_message': "Page Not Found!", "status_code": status_code})
+    response.status_code = status_code
+    return response
