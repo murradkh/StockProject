a449458c589ee822667b9535ecf86ba391c9b5fe
Malak-abuthaa <malak.abuthraa@gmail.com>
2020-10-14 10:36:01 +0300

    stock_background_job

---
+++ myapp/scheduler/scheduler.py
@@ -0,0 +1,8 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from .stock_api_update import stock_api_update
+
+
+def start():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(stock_api_update, 'interval', seconds=10)
+    scheduler.start()
