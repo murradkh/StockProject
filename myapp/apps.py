f0bba63d885829715c827c54690c37782c21c187
Malak-abuthaa <malak.abuthraa@gmail.com>
2020-10-14 10:36:01 +0300

    stock_background_job

--- myapp/apps.py
+++ myapp/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
-
+import sys
 
 class MyappConfig(AppConfig):
     name = 'myapp'
+
+    def ready(self):
+        if 'runserver' not in sys.argv:
+            return True
+        # you must import your modules here
+        # to avoid AppRegistryNotReady exception
+        from .models import Stock
+        from myapp import scheduler
+        scheduler.start()
