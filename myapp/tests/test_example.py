b49eec7c8da1da0f079211c8b7710d712c789309
Kamalq97 <45042524+kamalq97@users.noreply.github.com>
2020-10-15 14:33:01 +0300

    Update example_test.py
    An __init__.py file has also been added to this directory to help Django identify where the tests are located

--- myapp/tests/test_example.py
+++ myapp/tests/test_example.py
@@ -1,10 +1,9 @@
 from django.test import TestCase, Client
-
 from myapp.models import Stock
 
 
 class SingleStockTestCase(TestCase):
-    def setUp(self):
+    def setUp(self):            # NOTE: Optional function for setting up initial conditions
         Stock.objects.create(
             symbol="GE",
             name='General Electric',
@@ -14,7 +13,7 @@ class SingleStockTestCase(TestCase):
             change_percent=15.0
         )
 
-    def test_get(self):
+    def test_get_stock(self):   # NOTE: Test function name must always start with test_
         c = Client()
         response = c.get('/stock/GE/')
         self.assertEquals(response.status_code, 200)
