bf590311aee42b74948c3b6a7dabb128d5cfd211
Malak-abuthaa <malak.abuthraa@gmail.com>
2020-10-16 09:48:18 +0300

    stock_background_job

--- myrails/settings.py
+++ myrails/settings.py
@@ -78,7 +78,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
+    'OPTIONS': {
+
+    'timeout': 20,
+
+}
 }
 
 
