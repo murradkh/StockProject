324b1ab02a7d1fca77fe95f5b7a760adc830235e
murad <murradkhalil@gmail.com>
2020-10-17 05:53:54 -0700

    creating test files for different django aspects

--- myapp/tests/test_example.py
+++ myapp/tests/test_example.py
@@ -2,8 +2,11 @@ from django.test import TestCase, Client
 from myapp.models import Stock
 
 
-class SingleStockTestCase(TestCase):
-    def setUp(self):            # NOTE: Optional function for setting up initial conditions
+class SingleStockTestCase(TestCase):  # If your class doesn't use a database, use SimpleTestCase to inherit from
+    fixtures = ['stocks.json']
+
+    @classmethod
+    def setUpTestData(cls):  # allows the creation of initial data at the class level, once for the whole TestCase.
         Stock.objects.create(
             symbol="GE",
             name='General Electric',
@@ -13,7 +16,10 @@ class SingleStockTestCase(TestCase):
             change_percent=15.0
         )
 
-    def test_get_stock(self):   # NOTE: Test function name must always start with test_
-        client = Client()
-        response = client.get('/stock/GE/')
+    def setUp(self):  # NOTE: Optional function for setting up initial setups
+        pass
+
+    def test_get_stock(self):  # NOTE: Test function name must always start with test_
+        # this instance creation is uneeded when the clsas inherting from TestCase, it has a default one "self.client"
+        response = self.client.get('/stock/GE/')
         self.assertEqual(response.status_code, 200)
---
+++ myapp/tests/test_stock_api.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+
+class StockApiTestCase(TestCase):
+
+
+    def setUp(self):
+        self.existed_symbols = []
+        self.not_existed_symbols = []
+
+    def test_get_stock_info(self):
+        pass
+
+    def test_get_stock_historic_prices(self):
+        pass
+
+
