f230fbdc27a70aedfa75f7499a0fe440383ed1f9
murad <murradkhalil@gmail.com>
2020-10-18 05:10:15 -0700

    Adding Unit testing for "page_not_found" and "page_redirect" views

---
+++ myrails/test/test_views.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+
+class ViewsTestCase(TestCase):
+
+    def setUp(self):
+        self.not_existed_endpoints = ['/test/', "/test/test/", "/historic/", "/stock/", "/stock/AAL/test/",
+                                      "/historic/AAL/test/"]
+        self.endpoints_to_redirect = ['/test', '/test/test', '/historic', '/stock', '/stock/AAL/test', '/login']
+
+    def test_page_not_found(self):
+        for endpoint in self.not_existed_endpoints:
+            response = self.client.get(endpoint)
+            self.assertTemplateUsed(response, "exception.html")
+            self.assertEquals(response.status_code, 404)
+
+    def test_page_redirect(self):
+        for endpoint in self.endpoints_to_redirect:
+            response = self.client.get(endpoint)
+            self.assertRedirects(response, endpoint + '/', fetch_redirect_response=False)
