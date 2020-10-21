4de06d93415e96e4b9d39d7b43fcf88f6327727f
murad <murradkhalil@gmail.com>
2020-10-21 04:40:19 -0700

    resolving malak review issues

--- myapp/templatetags/myapp_tags.py
+++ myapp/templatetags/myapp_tags.py
@@ -1,10 +1,8 @@
-# from django import template
-#
-# from myapp.views import STOCKS_PER_PAGE
-#
-# register = template.Library()
-#
-#
-# @register.filter
-# def cal_index(counter, page_num, stocks_per_page_query):
-#     return (page_num-1) * int(stocks_per_page_query[stocks_per_page_query.index('=')+1:] or STOCKS_PER_PAGE) + counter
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag()
+def cal_index(counter, page_num, elem_per_page):
+    return (page_num - 1) * elem_per_page + counter
