4b4a5b1e023e745f9285716a3c1264dce10ce273
Kamalq97 <45042524+kamalq97@users.noreply.github.com>
2020-11-17 20:41:41 +0200

    Update base.js

--- myapp/static/js/myapp/base.js
+++ myapp/static/js/myapp/base.js
@@ -14,14 +14,36 @@ function getCookie(cname) {
     return "";
 }
 
-// view notifications
-$("#notificationsDropdown").on("show.bs.dropdown", function(event){
+
+function getUnreadCount(){
+    badge = document.getElementById('badgeCounter');
+    badge.innerHTML = "";
     var xhr = new XMLHttpRequest();
-    var url = myapp.URLS.notifications;
+    var url = myapp.URLS.notificationsUnreadCount;
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            var status = xhr.status;
+            if (status === 0 || (status >= 200 && status < 400)) {
+                var responseDict = JSON.parse(xhr.responseText);
+                var numNotifications = responseDict['unread_count']
+                if (numNotifications > 0){      
+                    badge.appendChild(document.createTextNode(numNotifications));
+                }
+            }
+        }
+    };
+    xhr.send();
+}
+
+
+$("#notificationsDropdown").on("show.bs.dropdown", function getNotifications(){
+    var xhr = new XMLHttpRequest();
+    var url = myapp.URLS.listNotifications;
     xhr.open("GET", url, true);
 
-    dropdown = document.getElementById('notificationsContainer');
-    dropdown.innerHTML = "";
+    nContainer = document.getElementById('notificationsContainer');
+    nContainer.innerHTML = "";
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
@@ -33,17 +55,24 @@ $("#notificationsDropdown").on("show.bs.dropdown", function(event){
                     paragraph.className = "text-center";
                     var text = document.createTextNode("No new notifications");
                     paragraph.appendChild(text);
-                    dropdown.prepend(paragraph);
+                    nContainer.prepend(paragraph);
                 }
                 else {
                     for (var key in responseDict) {
                         if (responseDict.hasOwnProperty(key)) {
                             var link = document.createElement("a");
+                            //link.attr("onclick",  markAsRead(responseDict[key]['pk']));
                             link.href = responseDict[key]['link'];
                             link.style.textDecoration = "none";
 
                             var div = document.createElement("div");
-                            div.className = "alert alert-secondary";
+                            if (responseDict[key]['is_read']){
+                                div.className = "alert alert-secondary";
+                            }
+                            else {
+                                div.className = "alert alert-primary";
+                            }
+                                
                             div.innerHTML = `<button type="button" class="close" 
                                             onclick="deleteNotifications(${responseDict[key]['pk']})" 
                                             data-dismiss="alert">×</button>`;
@@ -62,7 +91,7 @@ $("#notificationsDropdown").on("show.bs.dropdown", function(event){
                             div.appendChild(document.createElement("br"));
                             div.appendChild(time);
                             time.appendChild(timestamp);
-                            dropdown.prepend(link);
+                            nContainer.prepend(link);
                         }
                     }
                 }
@@ -70,7 +99,33 @@ $("#notificationsDropdown").on("show.bs.dropdown", function(event){
         }
     };
     xhr.send();
+    getUnreadCount();
 });
+
+
+function markAsRead(pk=""){
+    var xhr = new XMLHttpRequest();
+    if (pk === "") {
+        var url = myapp.URLS.notificationsAllRead; 
+    }
+    else {
+        var url = `../../../../notifications/${pk}/nread/`
+    }
+    xhr.open("POST", url, true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            var status = xhr.status;
+            if (status === 0 || (status >= 200 && status < 400)) {
+                console.log("all read");
+            }
+        }
+    };
+    xhr.setRequestHeader('X-CSRFToken', getCookie('csrftoken'));
+    xhr.send();
+    getUnreadCount();
+    getNotifications();
+}
 
 function deleteNotifications(pk=""){
     var xhr = new XMLHttpRequest();
@@ -87,13 +142,16 @@ function deleteNotifications(pk=""){
         if (xhr.readyState === XMLHttpRequest.DONE) {
             var status = xhr.status;
             if (status === 0 || (status >= 200 && status < 400)) {
-                console.log("success");
+                console.log("all deleted");
             }
         }
     };
     xhr.setRequestHeader('X-CSRFToken', getCookie('csrftoken'));
     xhr.send();
+    getUnreadCount();
+    getNotifications();
 }
+
 
 function parseDateTime(isoFormat) {
     date = new Date(isoFormat);
