47674b2e339b23b5d5559881c47892e4e591db7d
murad <murradkhalil@gmail.com>
2020-11-18 06:27:16 -0800

    creating test folder

--- myapp/scheduler/tests/test_scheduler.py
+++ myapp/scheduler/tests/test_scheduler.py
@@ -1,8 +1,9 @@
 from ..scheduler import scheduler
 import threading
 from django.test import TestCase
-from ..scheduler.stock_api_update import stock_api_update
-from ..models import Stock
+
+from ..stock_api_update import stock_api_update
+from ...models import Stock
 
 
 class SchedulerJobsTestCase(TestCase):
@@ -24,5 +25,3 @@ class JbsTestCase(TestCase):
         value_2 = Stock.objects.filter(top_rank=1)[0].last_modified
         self.assertNotEqual(value_1, value_2)
 
-
-#TODO: test the jobs here
