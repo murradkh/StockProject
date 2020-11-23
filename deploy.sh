f785a6eec95b6836c293b32774eba60d8383731b
Kamalq97 <45042524+kamalq97@users.noreply.github.com>
2020-11-24 00:17:39 +0200

    Fixed wrong file path

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 pkill python
 git pull
 pip install -r requirements.txt
-openssl rsautl -decrypt -inkey $PATH_TO_PRIVATE_KEY_PEM -in prod.encrypted -out prod.cfg
+openssl rsautl -decrypt -inkey $PATH_TO_PRIVATE_KEY_PEM -in myrails/configuration/prod.encrypted -out myrails/configuration/prod.cfg
 python manage.py migrate
 nohup python manage.py runserver 0.0.0.0:8000 &
