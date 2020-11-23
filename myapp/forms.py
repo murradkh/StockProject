7a9af1f16b387d2c9651b62f47e81df2cc6ab9b5
Kamalq97 <45042524+kamalq97@users.noreply.github.com>
2020-11-23 23:36:51 +0200

    Update forms.py

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from myapp.sub_models.notification_rules import ChangeStatusRule, ChangeThresholdRule, PriceThresholdRule, RecommendationAnalystRule
 
 
 class CustomRegistrationFrom(UserCreationForm):
@@ -43,3 +44,79 @@ class CustomChangePasswordForm(PasswordChangeForm):
     class Meta:
         model = User
         fields = ['old_password', 'new_password1', 'new_password2']
+
+
+class ChangeStatusRuleForm(forms.ModelForm):
+    num_of_days = forms.IntegerField(label='Number of days', initial=30, max_value=360, min_value=2,
+            help_text=f"Enter a number between 2 and 360")
+        
+    fired = forms.BooleanField(required=False, label='Do not let this rule send me notifications')
+
+    class Meta:
+        model = ChangeStatusRule
+        fields = ['watched_stock', 'status', 'num_of_days', 'fired']
+
+
+class ChangeThresholdRuleForm(forms.ModelForm):    
+    percentage_threshold = forms.FloatField(initial=0, max_value=100, min_value=-100,
+                            help_text="Enter a percentage between -100 and 100")
+
+    fired = forms.BooleanField(required=False, label='Do not let this rule send me notifications')
+    
+    class Meta:
+        model = ChangeThresholdRule
+        fields = ['watched_stock', 'when', 'percentage_threshold', 'fired']
+
+
+class PriceThresholdRuleForm(forms.ModelForm):    
+    price_threshold = forms.FloatField(initial=0, min_value=0,
+                      help_text="Enter a value equal to or larger than 0")
+
+    fired = forms.BooleanField(required=False, label='Do not let this rule send me notifications')
+
+    class Meta:
+        model = PriceThresholdRule
+        fields = ['watched_stock', 'when', 'price_threshold', 'fired']
+
+
+class RecommendationAnalystRuleForm(forms.ModelForm):   
+    threshold_recommenders_percentage = forms.FloatField(initial=1, min_value=1, max_value=100,
+                                        widget=forms.NumberInput(attrs={'class': 'form-control'}),
+                                        help_text="Enter a value between 1 and 100")
+    
+    fired = forms.BooleanField(required=False, label='Do not let this rule send me notifications')
+
+    class Meta:
+        model = RecommendationAnalystRule
+        fields = ['watched_stock', 'category', 'threshold_recommenders_percentage', 'fired']
+
+
+def get_rule_from_str (request, rule_type, pk=""):
+    instance = None
+    if rule_type == 'change_status':
+        if pk:
+            instance = get_instance(ChangeStatusRule.objects.filter(pk=pk)[:1])
+        return ChangeStatusRuleForm(request.POST or None, instance=instance), 'Status Change', instance
+
+    elif rule_type == 'change_threshold':
+        if pk:
+            instance = get_instance(ChangeThresholdRule.objects.filter(pk=pk)[:1])
+        return ChangeThresholdRuleForm(request.POST or None, instance=instance), 'Threshold Change', instance
+
+    elif rule_type == 'price_threshold':
+        if pk: 
+            instance = get_instance(PriceThresholdRule.objects.filter(pk=pk)[:1])
+        return PriceThresholdRuleForm(request.POST or None, instance=instance), 'Price Threshold', instance
+
+    elif rule_type == 'recommendation_analyst':
+        if pk: 
+            instance = get_instance(RecommendationAnalystRule.objects.filter(pk=pk)[:1])
+        return RecommendationAnalystRuleForm(request.POST or None, instance=instance), 'Analyst Recommendations', instance
+    else:
+        return None, None, None
+
+def get_instance (queryset):
+    if queryset.exists():
+        return queryset[0]
+    else:
+        return None
