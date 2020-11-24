5ae2d38d4fa4b3dfb17e6def6665af642e9728bd
Kamalq97 <45042524+kamalq97@users.noreply.github.com>
2020-11-24 19:17:37 +0200

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Stock, Profile
+from .models import Stock, Profile, SoldStock, BoughtStock
 
 admin.site.register(Stock)
 admin.site.register(Profile)
+admin.site.register(SoldStock)
+admin.site.register(BoughtStock)
