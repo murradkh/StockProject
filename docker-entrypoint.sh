7341fdf4395ba8de3c3c0df9f717f4510385de53
murad <murradkhalil@gmail.com>
2020-12-31 08:53:49 -0800

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,6 @@
 
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
+python manage.py stock_manager &
+python manage.py notifications_handler &
 gunicorn --workers=3 myrails.wsgi:application --bind 0.0.0.0:8000
